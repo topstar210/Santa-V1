7b475a98983441daa6f6f6bbeba3f209216c475f
topstar210 <happycamper0210@gmail.com>
2024-07-23 05:06:01 +0300

    feat: get registered users

--- src/components/users/Registered.tsx
+++ src/components/users/Registered.tsx
@@ -2,12 +2,13 @@ import { Chip, LinearProgress, Stack, Typography } from '@mui/material';
 import { DataGrid, GridApi, GridColDef, GridSlots, useGridApiRef } from '@mui/x-data-grid';
 import { ChangeEvent, useEffect, useState } from 'react';
 import SimpleBar from 'simplebar-react';
+import { fetchData } from 'services/apiService';
 
 import CustomDataGridFooter from 'components/common/table/CustomDataGridFooter';
 import CustomDataGridHeader from 'components/common/table/CustomDataGridHeader';
 import CustomDataGridNoRows from 'components/common/table/CustomDataGridNoRows';
 
-import { tableRowData, TableData } from 'data/users/registered';
+import { tableRowData } from 'data/users/registered';
 import dayjs from 'dayjs';
 
 export const tableColumns: GridColDef<tableRowData>[] = [
@@ -52,11 +53,23 @@ export const tableColumns: GridColDef<tableRowData>[] = [
 ];
 
 const RegisteredTable = () => {
+  const [tableData, setTableData] = useState<tableRowData[]>([]);
+
   const [searchText, setSearchText] = useState('');
   const apiRef = useGridApiRef<GridApi>();
 
   useEffect(() => {
-    apiRef.current.setRows(TableData);
+    const getData = async () => {
+      const { data, status } = await fetchData(`/users?is_temp=0`);
+      if (status) {
+        setTableData(data);
+      }
+    };
+    getData();
+  }, []);
+
+  useEffect(() => {
+    apiRef.current.setRows(tableData);
   }, [apiRef]);
 
   useEffect(() => {
@@ -67,7 +80,7 @@ const RegisteredTable = () => {
     const searchValue = event.currentTarget.value;
     setSearchText(searchValue);
     if (searchValue === '') {
-      apiRef.current.setRows(TableData);
+      apiRef.current.setRows(tableData);
     }
   };
 
@@ -114,7 +127,7 @@ const RegisteredTable = () => {
               onChange: handleChange,
               clearSearch: () => setSearchText(''),
             },
-            pagination: { labelRowsPerPage: TableData.length },
+            pagination: { labelRowsPerPage: tableData.length },
           }}
           initialState={{ pagination: { paginationModel: { page: 1, pageSize: 5 } } }}
           pageSizeOptions={[5, 10, 25]}
